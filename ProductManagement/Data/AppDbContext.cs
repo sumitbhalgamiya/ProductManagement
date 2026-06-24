9468375e6e2e3b36843e0aadf3911e23e501725e
Sumit <bhalgamiyasumit2001@gmail.com>
2026-06-25 00:20:58 +0530

    Product Management Project Setup

---
+++ ProductManagement/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductManagement.Models;
+
+namespace ProductManagement.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Products> Products { get; set; }
+        protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+        {
+            configurationBuilder
+                .Properties<decimal>()
+                .HavePrecision(18, 4);
+        }
+    }
+}
---
+++ ProductManagement/Models/Products.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProductManagement.Models
+{
+    public class Products
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Range(0.01, double.MaxValue)]
+        public decimal Price { get; set; }
+    }
+}
